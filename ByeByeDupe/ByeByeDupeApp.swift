8d0c98d06e9e26a3161824e3fbea72e1c070f892
Aarsh <aarshpatel2101@gmail.com>
2025-06-09 00:37:26 -0500

    Initial Commit

---
+++ ByeByeDupe/ByeByeDupeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByeByeDupeApp.swift
+//  ByeByeDupe
+//
+//  Created by Aarsh Patel on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ByeByeDupeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ByeByeDupeTests/ByeByeDupeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ByeByeDupeTests.swift
+//  ByeByeDupeTests
+//
+//  Created by Aarsh Patel on 6/9/25.
+//
+
+import Testing
+@testable import ByeByeDupe
+
+struct ByeByeDupeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
